91c72554fa6cd683bd88af06e276515bd6f74a73
Titoputraaa <titobamulo13@gmail.com>
2024-12-23 17:06:07 +0700

    stack structure

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    static int top = -1;
+    static int[] stack = new int[6];
+
+    static boolean isKosong() {
+        return top == -1;
+    }
+
+    static boolean isFull(int top, int[] stack) {
+        return top == stack.length-1;
+    }
+
+    static void push(int value) {
+        if (isFull(top, stack)) {
+            System.out.println("stack full, tidak dapat menambahkan value baru");
+        } else {
+            stack[++top] = value;
+            System.out.println("menambahkan value " + value + " top menjadi : " + top);
+        }
+    }
+
+    static void pop() {
+        if(isKosong()) {
+            System.out.println("stack kosong, tidak dapat menghapus stack");
+        } else {
+            System.out.println("menghapus value stack " + stack[top]);
+            stack[top--] = 0;
+            System.out.println("top menjadi " + top);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < ; i++) {
+            
+        }
+        System.out.println(Arrays.toString(stack));
+    }
+}
